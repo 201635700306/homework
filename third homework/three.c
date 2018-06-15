75e5bea982af1e1317985cd59eaf34c5e27d7148
郭梦婷 <38374135+201635700306@users.noreply.github.com>
2018-06-15 23:19:15 +0800

    Add files via upload

---
+++ third homework/three.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <conio.h>
+int main(){
+  int a[3][3];
+  int i,j;
+  int sum=0;
+  for(i=0;i<3;i++)
+    for(j=0;j<3;j++){
+      scanf("%d",&a[i][j]);
+    }
+  for(i=0;i<3;i++)
+    for(j=0;j<3;j++){
+      if((i==j)||(i+j==2))
+        sum=sum+a[i][j];
+    }
+  printf("%d",sum);
+  getch();
+  return 0;
+} 
